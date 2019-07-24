066e418f75da71de9c267de7720a7d38ddd397f9
ABow <eezzlife@gmail.com>
2019-07-24 12:24:40 +0800

    no message

--- app.py
+++ app.py
@@ -41,7 +41,8 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text='ABC'))
+        TextSendMessage(text='ABCD'))
+
 
 def getPhoto():
     insta_url = 'https://www.instagram.com/explore/tags/realyami/'
